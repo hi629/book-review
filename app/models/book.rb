71fcbd449e22ccf538fcedb8c25c66089436275a
大平裕章 <hirojoy189@gmail.com>
2018-03-17 11:39:15 +0900

    basic review crud

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ApplicationRecord
 belongs_to :user
 belongs_to :category
+has_many :reviews
 # book_image となっているところは好きな名前で良い
 has_attached_file :book_image, styles: { book_index: "250x350>", book_show: "325x475>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :book_image, content_type: /\Aimage\/.*\z/
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # resourceful route
-  resources :books
+  resources :books do
+    resources :reviews
+  end
   
   root 'books#index'
 end
